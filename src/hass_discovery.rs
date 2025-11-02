656a77cc16d24a177aae0a7c6b9207bdecdc23d8
Sebastian Schmidt <isibboi@gmail.com>
2025-11-02 14:37:29 +0200

    Fix device class error.

--- src/hass_discovery.rs
+++ src/hass_discovery.rs
@@ -54,6 +54,12 @@ QoS: 0
 {"name": "Ruuvi 346C", "id": "D4:D8:D8:CB:34:6C", "rssi": -84, "brand": "Ruuvi", "model": "RuuviTag", "model_id": "RuuviTag_RAWv2", "type": "ACEL", "tempc": -19.575, "tempf": -3.235, "hum": 60.7725, "pres": 1010.48, "accx": 0.0196133, "accy": -0.0784532, "accz": -1.03558224, "volt": 2.595, "tx": 4, "mov": 33, "seq": 37604, "mac": "D4:D8:D8:CB:34:6C"}
 */
 
+/*
+Error 'expected SensorDeviceClass or one of 'date', 'enum', 'timestamp', 'absolute_humidity', 'apparent_power', 'aqi', 'area', 'atmospheric_pressure', 'battery', 'blood_glucose_concentration', 'carbon_monoxide', 'carbon_dioxide', 'conductivity', 'current', 'data_rate', 'data_size', 'distance', 'duration', 'energy', 'energy_distance', 'energy_storage', 'frequency', 'gas', 'humidity', 'illuminance', 'irradiance', 'moisture', 'monetary', 'nitrogen_dioxide', 'nitrogen_monoxide', 'nitrous_oxide', 'ozone', 'ph', 'pm1', 'pm10', 'pm25', 'pm4', 'power_factor', 'power', 'precipitation', 'precipitation_intensity', 'pressure', 'reactive_energy', 'reactive_power', 'signal_strength', 'sound_pressure', 'speed', 'sulphur_dioxide', 'temperature', 'volatile_organic_compounds', 'volatile_organic_compounds_parts', 'voltage', 'volume', 'volume_storage', 'volume_flow_rate', 'water', 'weight', 'wind_direction', 'wind_speed'
+for dictionary value @ data['device_class']' when processing MQTT discovery message topic: 'homeassistant/sensor/C6CFC7267C2B-dewpoint/config',
+message: '{'state_class': 'measurement', 'device': {'via_device': 'RuuviTag Additions', 'connections': [['mac', 'C6CFC7267C2B']], 'identifiers': ['C6CFC7267C2B'], 'model': 'RuuviTag_RAWv2', 'name': 'RuuviTag-267C2B', 'manufacturer': 'Ruuvi'}, 'unique_id': 'C6CFC7267C2B-dewpoint2', 'device_class': 'dewpoint', 'value_template': '{{ value_json.dewpoint | is_defined }}', 'name': 'RuuviTag_RAWv2-dewpoint', 'state_topic': 'home/Radd/RuuviTagAdditions/C6CFC7267C2B', 'unit_of_measurement': '°C'}'
+*/
+
 impl HassDiscoveryMessages {
     pub fn new(config: &Config, ruuvi_message: &RuuviMessage) -> Self {
         let device_id = ruuvi_message.id.replace(':', "");
@@ -67,12 +73,11 @@ impl HassDiscoveryMessages {
     }
 
     pub fn iter_messages(&self) -> impl Iterator<Item = anyhow::Result<Message>> {
-        [("dewpoint", "°C")]
-            .into_iter()
-            .map(|(measurement, unit_of_measurement)| {
+        [("dewpoint", "temperature", "°C")].into_iter().map(
+            |(measurement, device_class, unit_of_measurement)| {
                 let payload = HassDiscoveryPayload {
                     stat_t: format!("home/Radd/RuuviTagAdditions/{}", self.device_id),
-                    dev_cla: measurement.to_string(),
+                    dev_cla: device_class.to_string(),
                     unit_of_meas: unit_of_measurement.to_string(),
                     state_class: "measurement".to_string(),
                     name: format!("{}-{}", self.model_id, measurement),
@@ -99,6 +104,7 @@ impl HassDiscoveryMessages {
                     .qos(QoS::AtMostOnce)
                     .payload(payload)
                     .finalize())
-            })
+            },
+        )
     }
 }
