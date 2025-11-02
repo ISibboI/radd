42931f918fd8aaeeaaae6b8aa01b193773c2b3fa
Sebastian Schmidt <isibboi@gmail.com>
2025-11-02 09:48:43 +0200

    Init logging.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use log::LevelFilter;
+use simplelog::{ColorChoice, TermLogger, TerminalMode};
+
+fn init_logging() {
+    TermLogger::init(LevelFilter::Debug, Default::default(),TerminalMode::Mixed, ColorChoice::Auto).unwrap();
+}
+
 fn main() {
+    init_logging();
+    
     println!("Hello, world!");
 }
