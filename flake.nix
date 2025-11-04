9b5fcce7a260738b6e837786ca0abc716f00c9ea
Sebastian Schmidt <isibboi@gmail.com>
2025-11-04 19:27:40 +0200

    Fix.

--- flake.nix
+++ flake.nix
@@ -140,7 +140,7 @@
                     MQTT_PASSWORD=`cat ${cfg.mqttPasswordFile} | xargs echo` # Remove whitespace before and after password
                     MQTT_LISTEN_TOPIC="${cfg.mqttListenTopic}"
                     MQTT_HASS_DISCOVERY_TOPIC="${cfg.mqttHomeAssistantDiscoveryTopic}"
-                    ${self.outputs.package}/bin/radd
+                    ${cfg.package}/bin/radd
                   '
                 '';
                 Restart = "on-failure";
